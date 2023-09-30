999858ac198d1052da028ba51e335e3865be19a2
Glitch (keyboard-by-sosa) <none>
2023-09-30 01:51:59 +0000

    keyboard-by-sosa

--- script.js
+++ script.js
@@ -6,7 +6,6 @@
     blackscreen = document.querySelector(".blackScreen");
 
 
-
 let allKeys = [],
     audio = new Audio(`Psamples/C2.ogg`);
 
@@ -20,7 +19,6 @@ const playTune = (key) => {
     audio.play();
   };
 
-
   const pressedKey = (e) => {
     if (allKeys.includes(e.key)) playTune(e.key);
   };
@@ -37,69 +35,8 @@ const playTune = (key) => {
   audio.volume = e.target.value;
 };
 
+document.addEventListener("click", pressedKey);
 keysCheckbox.addEventListener("click", hideKeys);
 volumeSlider.addEventListener("input", handleVolume);
 powerBox.addEventListener("click", power);
-document.addEventListener("click", pressedKey);
 
-/*
-const showK = async function (event) {
-  const pKeys = document.querySelectorAll(".pKeys .key"),
-    volumeSlider = document.querySelector(".volume-slider input"),
-    keysCheckbox = document.querySelector(".keys-checkbox input"),
-    powerBox = document.querySelector(".power input"),
-    blackscreen = document.querySelector(".blackScreen");
-  const hideKeys = () => {
-    pKeys.forEach((key) => key.classList.toggle("hide"));
-  };
-};
-const powerB = async function (event) {
-  const pKeys = document.querySelectorAll(".pKeys .key"),
-    volumeSlider = document.querySelector(".volume-slider input"),
-    keysCheckbox = document.querySelector(".keys-checkbox input"),
-    powerBox = document.querySelector(".power input"),
-    blackscreen = document.querySelector(".blackScreen");
-  const power = () => {
-    blackscreen.classList.toggle("hide");
-  };
-};
-
-const volumeS = async function (event) {
-  let audio = new Audio(`Psamples/C2.ogg`);
-  const handleVolume = (e) => {
-  audio.volume = e.target.value;
-};
-}
-
-const keysC = async function (event) {
-  const pKeys = document.querySelectorAll(".pKeys .key");
-  
-  let allKeys = [],
-    audio = new Audio(`Psamples/C2.ogg`);
-  const playTune = (key) => {
-    audio.src = `Psamples/${key}.ogg`;
-    audio.play();
-  };
-  pKeys.forEach((key) => {
-    allKeys.push(key.dataset.key);
-    key.addEventListener("click", () => playTune(key.dataset.key));
-  });
-  const pressedKey = (e) => {
-    if (allKeys.includes(e.key)) playTune(e.key);
-  };
-};
-
-
-window.onload = function () {
-  const sKey = document.querySelector(".keys-checkbox input");
-  sKey.onclick = showK;
-
-  const pButton = document.querySelector(".power input");
-  pButton.onclick = powerB;
-  
-  const vSlider = document.querySelector(".volume-slider input");
-  vSlider.onclick = volumeS;
-
-  const keysClicked = document.querySelectorAll(".pKeys .key");
-  keysClicked.onclick = keysC;
-};*/
--- server.js
+++ server.js
@@ -6,5 +6,17 @@ app.use(express.static("views"));
 app.use(express.static("./"));
 app.use(express.json());
 
+const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
+
+app.use(expressCspHeader({
+    directives: {
+        'default-src': [SELF],
+        'script-src': [SELF, INLINE],
+        'style-src': [SELF],
+        'img-src': [SELF],
+        'worker-src': [NONE],
+        'block-all-mixed-content': true
+    }
+}));
 
 app.listen(3000);
